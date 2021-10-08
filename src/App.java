29111c28d44be0fcbf783ae4c8795b993123f821
HelisMar <helismartoja@gmail.com>
2021-10-08 18:29:42 +0300

    tictactoe

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("1|2|3");
+        System.out.println("-----");
+        System.out.println("4|5|6"); 
+        System.out.println("-----");
+        System.out.println("7|8|9");
+        
     }
 }
