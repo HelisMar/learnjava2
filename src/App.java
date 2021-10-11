e99b3ea50ff90226cc0a08f6db3c300e2136405c
HelisMar <helismartoja@gmail.com>
2021-10-11 18:02:32 +0300

    jama.ära vaata. ei tööta

--- src/App.java
+++ src/App.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1|2|3");
-        System.out.println("-----");
-        System.out.println("4|5|6"); 
-        System.out.println("-----");
-        System.out.println("7|8|9");
-        
+       
+        int[] nums = new int [10]; //1mõõtmelised arrayd. Tuleb sisestada väärtus(10)
+        nums [0] = 2;
+        String[] names = {"helis", "heidi", "tormi"};
+        System.out.printIn(names) ;
+        String[] names2 = {"helis", "heidi", "tormi ja sven"};
+        System.out.printIn(names2[0]);
+        for (String name : names2){
+            System.out.printIn (name);
+        }
+        ArrayList<String> fruits = new ArrayList<String>();
+        fruits.add("banana");
+        fruits.add("apple");
+        fruits.add("pineapple");
+        System.out.printIn(fruits.get(2));
+        int[][] array2d = new int [3][3];        //kahemõõtmelised arrayd
+        array2d [0][0]= 3;                       //kohal 0,0 asub 3
+        array2d [2][1]= 4;                       //kohal 2,1 asub 4
+        System.out.printIn(array2d [0][0]);         //kui pringin välja 00 koha, siis tuleb 3
+
+
+        Sring[][] board = {
+            {"1","2","3"},
+            {"4", "5" ,"6"},
+            {"7", "8", "9"}
+        };
+
     }
 }
