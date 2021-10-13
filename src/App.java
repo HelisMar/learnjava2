d4978395988ef1ce9527be2ab3227bcae8823023
HelisMar <helismartoja@gmail.com>
2021-10-13 17:46:50 +0300

    Massiivid, arrayd, 2D

--- src/App.java
+++ src/App.java
@@ -1,50 +1,26 @@
+import java.util.ArrayList;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-       Animal animal1 = new Animal ("Miki Hiir", 56, 2); //on animal tüüpi, nimi animal1 /animal onbjekst on animal klassi tüüpi. saame luua uue animali new
-       Animal.nr =1;
-       //animal1.name = "Miki Hiir";
-        //animal1.age = 56;
-        //animal1.nLegs = 2;
-        animal1.getDescription();
 
-        Animal animal2 = new Animal ("Iiah", 5, 4 ); //saab lisada animal 2 ja siis saab mõlemat välja pritida. Tegevus sama, omadus sama aga eraldi seisvad andmed
-        Animal.nr = 2;
-        //animal2.name = "Iiah";
-        //animal2.age = 5;
-        //animal2.nLegs = 4;
-        animal2.getDescription();
-        System.out.println(Animal.nr);
-        System.out.println(Animal.nr);
+        int[] nums = new int[10]; //peab olema suurus ette määratud -10-
+            nums[0] = 16;
+            nums[1] = 20;
+            nums[2] = 21;
+            nums[4] = 22;
 
-    }
-}
+            System.out.println(nums[1]); //Välja saab pritida, kui lisad väärtuse kandilistega
+            
+            String[] names = {"helis", "heidi"}; //kui suurust ei ole ette määratud, saab ta aru ka mitu on loogsulgudes 0,1,---
 
- class Animal {
-    public String name;  //Need on näited atribuutidest e propertitest
-    public int age;
-    public int nLegs; //number of legs
-    public static int nr;
+            System.out.println(names[1]);
 
-    public Animal(String name, int age, int nLegs) {  //constructor - see lubab meil käituda lihtsamalt. Ei pea eraldiseisvalt ära täita, nõuab, et me üleval midagi ära täidaksime ja ei ole vaja eraldi name ja aega ära täita
-        this.name = name;
-        this.age = age;
-        this.nLegs= nLegs;
-    }
-
-    public void getDescription (){
-        System.out.printf("My Name is %s and I am %d years old and I have %d legs \n", this.name, this.age, this.nLegs);  //meetod mida ta on võimeline tegema (void, sest ei tagasta midagi)
-    }
-
-            public int getAge() {
-                return this.age;
+            for(int num : nums){
+                System.out.println(num);
             }
 
-            public void setAge(int age) {
-                if(age>=0){
-                    this.age =age;
-
-                }
+            }
             
-                
         }
-    }  
