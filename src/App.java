9c33bd61aa8ba58b6e5cd48b92e18da93a167d66
HelisMar <helismartoja@gmail.com>
2021-11-22 20:17:01 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,43 +1,21 @@
 import java.time.LocalDate;
 import java.util.Calendar;
 
-public class App {
-    public static void main(String[] args) throws Exception {
 
-            Student student = new Student ("Helis", LocalDate.of(1990, 03, 21));
-            
-    }
-            
+    public class App {
+        public static void main (String [] args) {
+        int temp = 0;
+    
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                temp = temp + i;
+            }
+    
         }
     
-class Person {                 //Koolides on õpilased ja õpetajad eraldi aga neil on ühised näitajad - mõlemad on isikud
-    private String name;        //neil on mingid muutujad nii töötajatel kui ka õpilastel
-    private LocalDate birthdate;
-
-    public Person (String name, LocalDate birthdate){
-        this.name = name;
-        this.birthdate = birthdate; }
-
-        
-
-            Public String getName(){
-                return name;
-            }
-            public LocalDate getBirthDate () {
-                return birthdate;
-            }
+        System.out.println (temp);
+       }
+    
+ 
         }
-        class Student extends Person {                          // extendiga saab seda laiendada
-            private Integer missedClasses;
-            public Student (String name, LocalDate birthdate) {
-                super(name,birthdate);
-            }
-            public Integer getMissedClasses(){
-                return missedClasses;
-            }
-            public void setMissedClasses(Integer missedClasses){
-                this.missedClasses = missedClasses;
 
-            }
-        }
-}
